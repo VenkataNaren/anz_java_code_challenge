89be6e0b69cc4d4ff929f91b3e81ca2acfe6dd6f
vk1679 <venkata.kurapati@accenture.com>
2026-01-18 10:31:59 +1300

    Initial commit - ANZ Java Code Challenge

---
+++ src/main/java/com/anz/challenge/AnzJavaCodeChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.anz.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnzJavaCodeChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnzJavaCodeChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anz/challenge/AnzJavaCodeChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anz.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnzJavaCodeChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
